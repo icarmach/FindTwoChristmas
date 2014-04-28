3b5dac62921c70b16bcde595167b2209174383b9
Ignacio Carmach <icarmach@gmail.com>
2014-04-27 23:50:10 -0400

    UI

--- FindTwoChristmas/FindTwoChristmas/MainWindow.xaml.cs
+++ FindTwoChristmas/FindTwoChristmas/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,9 +21,60 @@ namespace FindTwoChristmas
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<string> premiosPendientes;
+        private List<string> premiosEntregados;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            premiosPendientes = new List<string>();
+            premiosEntregados = new List<string>();
+
+            premiosPendientes.Add("1");
+            premiosPendientes.Add("1");
+            premiosPendientes.Add("2");
+            premiosPendientes.Add("2");
+            premiosPendientes.Add("3");
+            premiosPendientes.Add("3");
+            premiosPendientes.Add("4");
+            premiosPendientes.Add("4");
+            premiosPendientes.Add("5");
+            premiosPendientes.Add("5");
+            premiosPendientes.Add("6");
+            premiosPendientes.Add("6");
+            premiosPendientes.Add("7");
+            premiosPendientes.Add("7");
+            premiosPendientes.Add("8");
+            premiosPendientes.Add("8");
+            premiosPendientes.Add("special");
+            premiosPendientes.Add("ghost");
+            premiosPendientes.Add("ghost");
+            premiosPendientes.Add("car");
+            premiosPendientes.Add("car");
+
+            RandomizePrizes();
+        }
+
+        public void RandomizePrizes()
+        {
+            Random rng = new Random();
+            int n = premiosPendientes.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                string value = premiosPendientes[k];
+                premiosPendientes[k] = premiosPendientes[n];
+                premiosPendientes[n] = value;
+            }  
+        }
+
+        private void FlipA1_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            RandomizePrizes();
+
+            FlipA1.BackSide = 
         }
     }
 }
